1681b0245add8dfe3406be12ec44df0fcc383f1f
koji toukubo <toukubo@gmail.com>
2025-03-16 17:31:34 +0900

    local version

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const MarkdownToBlocks = require('@tryfabric/martian');
-const MarkdownToBlocks = '# Hello Notion!';
+const fs = require('fs');
+
+const data = fs.readFileSync('example.txt', 'utf8');
+console.log(data);
+const markdown = data;
 const blocks = MarkdownToBlocks.markdownToBlocks(markdown);
 console.log(blocks);
