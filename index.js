9445690c6d9c75c28e7af79ca91eee014ed06bd3
koji toukubo <toukubo@gmail.com>
2025-03-16 17:26:08 +0900

    local function. yeah.

---
+++ index.js
@@ -0,0 +1,4 @@
+const MarkdownToBlocks = require('@tryfabric/martian');
+const MarkdownToBlocks = '# Hello Notion!';
+const blocks = MarkdownToBlocks.markdownToBlocks(markdown);
+console.log(blocks);
